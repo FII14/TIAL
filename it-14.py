0ed2a23711fab666124a5d4a380d373c6b3f4b80
FII14 <121723975+FII14@users.noreply.github.com>
2023-06-04 16:42:02 +0700

    Update it-14.py

--- it-14.py
+++ it-14.py
@@ -1,12 +1,27 @@
 import argparse
 import requests
+from colorama import Fore
 import os
 
+r=Fore.RED
+w=Fore.RESET
+g=Fore.GREEN
+
+os.system("clear")
+
 # Create the argument parser
+print(f"""{r}
+ ._..__   .___..__ .__. __ .  ..___.__
+  | [__)    |  [__)[__]/  `|_/ [__ [__)
+ _|_|       |  |  \|  |\__.|  \[___|  \.
+{w}""")
 parser = argparse.ArgumentParser(description='Track IP location.')
 
 # Add the argument for IP address
-parser.add_argument('-i', '--ip', type=str, help='The IP address to track')
+parser.add_argument('--ip', type=str, help='The IP address to track')
+
+# Add the argument for output file
+parser.add_argument('--output', type=str, help='The output file to save the location information')
 
 # Parse the arguments from the command line
 args = parser.parse_args()
@@ -26,7 +41,7 @@ if response.status_code != 200:
     print("Failed to track IP location.")
 else:
     data = response.json()
-    
+
     # Check if the IP address was found
     if data['status'] == 'fail':
         print("IP address not found.")
@@ -40,18 +55,30 @@ else:
         lon = data['lon']
         isp = data['isp']
 
-        os.system("clear")
-
         # Display the complete location information
-        print("")
-        print("-------------------------------------------")
+        print("-----------------------------------------")
         print(f" IP Location Information: {ip_address}")
-        print("-------------------------------------------")
+        print("-----------------------------------------")
         print(f" City: {city}")
         print(f" Region: {region}")
         print(f" Country: {country}")
         print(f" ZIP Code: {zip_code}")
         print(f" Coordinates: {lat}, {lon}")
         print(f" ISP: {isp}")
-        print("-------------------------------------------")
-        print("")
+        print("-----------------------------------------")
+        print(f"                           {g}Code by: FII14{w}")
+        # Check if the output file argument is provided
+        if args.output:
+            output_file = args.output
+            with open(output_file, 'w') as file:
+                file.write("-----------------------------------------\n")
+                file.write(f"IP Location Information: {ip_address}\n")
+                file.write("-----------------------------------------\n")
+                file.write(f"City: {city}\n")
+                file.write(f"Region: {region}\n")
+                file.write(f"Country: {country}\n")
+                file.write(f"ZIP Code: {zip_code}\n")
+                file.write(f"Coordinates: {lat}, {lon}\n")
+                file.write(f"ISP: {isp}\n")
+                file.write("---------------------------------------\n")
+                file.write("                         Code by: FII14\n")
